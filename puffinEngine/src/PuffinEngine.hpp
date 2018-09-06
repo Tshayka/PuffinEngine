a9041532555b3a0a949f5a518f2f41d04b32229c
Damian Bomba <damemibomba@gmail.com>
2018-09-06 09:37:02 +0200

    Basic initialization functions

--- puffinEngine/src/PuffinEngine.hpp
+++ puffinEngine/src/PuffinEngine.hpp
@@ -1,6 +1,24 @@
 #pragma once
 
+#include "Device.hpp"
+
 class PuffinEngine{
     public:
+    PuffinEngine();
+	~PuffinEngine();
+
+    void InitWindow();
+    void Run();
+
     std::string isUppercase(char l);
+
+    private:
+    std::shared_ptr<Device> world_device = nullptr;
+    GLFWwindow* window;
+
+    void CleanUp();
+    void CreateDevice();
+    void DestroyDevice();
+    void InitVulkan();
+    void MainLoop();
 };
