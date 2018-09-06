f1e10df628e0763c402d17dd177584a5f5e0e152
Damian Bomba <damemibomba@gmail.com>
2018-09-06 09:34:50 +0200

    Basic init, device, extensions, instance

--- puffinEngine/src/Device.hpp
+++ puffinEngine/src/Device.hpp
@@ -1,15 +1,31 @@
 #pragma once
 
 #include <vector>
-#include <chrono> 
 #include <memory>
 
 #define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h> 
+#include <GLFW/glfw3.h> //#include <vulkan/vulkan.h> is not needed
+
+#include "Buffer.cpp"
 
 class Device
 {
 public:
 	Device();
-    ~Device();
+	~Device();
+    
+    VkDevice device = VK_NULL_HANDLE;
+
+    void InitDevice(GLFWwindow*);
+    void DeInitDevice();
+
+private:
+    
+    VkInstance instance = VK_NULL_HANDLE;
+	GLFWwindow* window;
+
+	void CreateInstance();
+    std::vector<const char*> GetRequiredExtensions();
+
+    const std::vector<const char*> validationLayers = {"VK_LAYER_LUNARG_standard_validation"};
 };
