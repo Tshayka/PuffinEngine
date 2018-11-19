6f5ea252d78e0f1d539689e0e6dfd4344c1bd74f
Damian Bomba <damemibomba@gmail.com>
2018-11-19 10:05:43 +0100

    Add basic jumping method

--- puffinEngine/src/Character.cpp
+++ puffinEngine/src/Character.cpp
@@ -24,3 +24,51 @@ void Character::Init(unsigned int maxHealth, int currentHealth, unsigned int gol
 	this->currentHealth = currentHealth;
 	this->gold = gold;
 }
+
+void Character::StartJump() {
+	if (onGround) {
+    	movementGoal.y = 30.0f;
+		onGround = false;
+	}
+}
+
+void Character::EndJump() {
+    if(movementGoal.y > 15.0f)
+		movementGoal.y = -10.0f;
+}
+
+void Character::UpdatePosition(float dt) {
+	movement.x = Approach(movementGoal.x, movement.x, dt * 80);
+	movement.y = Approach(movementGoal.y, movement.y, dt * 80);
+ 	movement.z = Approach(movementGoal.z, movement.z, dt * 80);
+
+	movement += gravity * dt;
+	position += movement * dt;
+
+	//std::cout << onGround << " " << position.y << " " << movementGoal.y << std::endl;
+
+	if (position.y < groundLevel){
+		position.y = groundLevel;
+		movementGoal.y = 0.0f;
+		onGround = true;
+	}
+}
+
+
+// void Camera::UpdatePosition(float dt) {
+// 	direction = glm::vec3(cos(pitch) * sin(yaw), sin(pitch), cos(pitch) * cos(yaw));
+
+// 	// Smooth movement and edge case in approach, without is movement is const
+// 	movement.x = Approach(movementGoal.x, movement.x, dt * 80);
+// 	movement.y = Approach(movementGoal.y, movement.y, dt * 80);
+// 	movement.z = Approach(movementGoal.z, movement.z, dt * 80);
+
+// 	foward = direction;
+// 	foward.y = 0.0f;
+// 	glm::normalize(foward);
+
+// 	velocity = foward * movement.x + glm::cross(up, foward) * movement.z;
+// 	velocity.y = movement.y;
+// 	position += velocity * dt;
+// 	view = position + direction; 
+// }
--- puffinEngine/src/Character.hpp
+++ puffinEngine/src/Character.hpp
@@ -2,18 +2,26 @@
 
 #include "Actor.hpp"
 
+static float groundLevel = 1.0f;
+
 class Character : public Actor {
 public:
 	Character(std::string name, std::string description, glm::vec3 position);
 	virtual ~Character();
 
 	void Init(unsigned int maxHealth, int currentHealth, unsigned int gold);
-    
+	void StartJump();
+	void EndJump();
+    void UpdatePosition(float) override;
+
 	unsigned int maxHealth;
 	int currentHealth;
 	unsigned int gold;
     ActorType type = ActorType::Character;
+	bool onGround = false;
+
+	glm::vec3 gravity = glm::vec3(0.0f, -10.0f, 0.0f);
 
 private:
-	std::string albedoTexture = "puffinEngine/assets/textures/icons/cameraIcon.jpg";
+	std::string albedoTexture = "puffinEngine/assets/textures/icons/cameraIcon.jpg";	
 };
